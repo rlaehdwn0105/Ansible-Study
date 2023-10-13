915e282d48dae5592bc43110bf5e7c67b7813784
ansible <ansible@ansible.example.com>
2023-10-13 11:08:13 +0900

    first commit

---
+++ project/adhoc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ 
+ ansible all -m yum_repository -b \
+ -a "name=MyBase description='base software' baseurl='http://192.168.10.10/pkg/BaseOS' gpgcheck=no"
+
+ ansible all -m yum_repository -b \
+ -a "name=MyAppStream description='Appstream software' baseurl='http://192.168.10.10/pkg/AppStream' gpgcheck=no"
